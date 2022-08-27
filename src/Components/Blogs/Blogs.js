b7324ad5e5d3a12a3420ec64461cf28b6b276dde
GMShimon <simon.rosedale99@gmail.com>
2022-08-27 07:40:37 +0600

    question answer

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -3,7 +3,11 @@ import './Blogs.css';
 const Blogs = () => {
     return (
         <div>
-            <h1>Blog sections</h1>
+            <h1 className="text-center text-4xl my-4">Question Answer</h1>
+            <p className="text-xl">1. Without having to manually transmit data through props, the Context API may be used to communicate data with numerous components. The Context API is appropriate for use cases like theming, user language, authentication, etc.</p>
+            <p className="text-xl">
+            The HTML semantics refers to the tags that provide meaning to an HTML page rather than just presentation. It makes HTML more comprehensible by better defining the different sections and layout of web pages.
+            </p>
         </div>
     );
 };
